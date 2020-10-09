eb4549dd8616656897c1ec8395f11d1f8e950b0d
Josué Gimenes <josuegalvares@gmail.com>
2020-10-09 19:33:16 -0300

    Finishing tests at login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -43,6 +43,7 @@ const SignIn: React.FC = () => {
         await schema.validate(data, {
           abortEarly: false,
         });
+
         await signIn({
           email: data.email,
           password: data.password,
@@ -76,7 +77,7 @@ const SignIn: React.FC = () => {
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu logon</h1>
 
-            <Input name="email" icon={FiMail} placeholder="Email" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
             <Input
               name="password"
               icon={FiLock}
